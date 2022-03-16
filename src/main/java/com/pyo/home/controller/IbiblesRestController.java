99b7f92143fd537d78a0000050c689432ed908ac
unknown <ms147850@gmail.com>
2022-03-16 18:15:25 +0900

    add exception

--- src/main/java/com/pyo/home/controller/IbiblesRestController.java
+++ src/main/java/com/pyo/home/controller/IbiblesRestController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pyo.home.DTO.Bible;
+import com.pyo.home.exception.ContentsException;
 import com.pyo.home.repository.IbibleRepository;
 
 @RestController
@@ -21,7 +24,16 @@ public class IbiblesRestController {
 	@GetMapping("/{contents}")
 	public List<Bible> retrieveContents(@PathVariable String contents) throws Exception{
 		List<Bible> biblelist = ibibleRepository.findAllContents(contents, Sort.by(Sort.Direction.ASC,"id"));
+		
+		if(biblelist.size()<=0)
+			throw new ContentsException(String.format("Not Exist \'[%s]\'", contents));
+		
 		return biblelist;
+	}
+	
+	@GetMapping("/all")
+	public List<Bible> retrieveAll() throws Exception{
+		return ibibleRepository.findAll();
 	}
 
 }
