4b8289d8dda0b1127045206eef7e9096fe4bb33e
unknown <ms147850@gmail.com>
2022-03-17 23:20:08 +0900

    update HATEOS

--- src/main/java/com/pyo/home/controller/IbiblesRestController.java
+++ src/main/java/com/pyo/home/controller/IbiblesRestController.java
@@ -1,11 +1,15 @@
 package com.pyo.home.controller;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +26,18 @@ public class IbiblesRestController {
 	IbibleRepository ibibleRepository;
 	
 	@GetMapping("/{contents}")
-	public List<Bible> retrieveContents(@PathVariable String contents) throws Exception{
+	public CollectionModel<Bible> retrieveContents(@PathVariable String contents) throws Exception{
 		List<Bible> biblelist = ibibleRepository.findAllContents(contents, Sort.by(Sort.Direction.ASC,"id"));
-		
 		if(biblelist.size()<=0)
 			throw new ContentsException(String.format("Not Exist \'[%s]\'", contents));
 		
-		return biblelist;
+		// HATEOS 
+		CollectionModel<Bible> model = CollectionModel.of(biblelist);
+		WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(
+				WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAll());
+		model.add(linkTo.withRel("all-contents")); // return all-contents ref
+		
+		return model;
 	}
 	
 	@GetMapping("/all")
